cca579e6c855c947e84dfa21e003785a7874f779
sahmnlama <sahan.lamahewa@gmail.com>
2018-03-16 09:48:04 +0530

    first commit

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -9,27 +9,12 @@ function aaa1(){
 // device APIs are available
 //
 function onDeviceReady() {
-/*
-  // Enable background mode
-  cordova.plugins.backgroundMode.enable();
-  // Android customization
-  cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
 
-  cordova.plugins.backgroundMode.onfailure = function(errorCode) {};
 
-  // Called when background mode has been activated
-  cordova.plugins.backgroundMode.onactivate = function () {
 
-  setTimeout(function () {
-  // Modify the currently displayed notification
-    cordova.plugins.backgroundMode.configure({
-        text:'Running in background for more than 5s now.',
-        hidden: false,
-        resume: true
-      });
-  }, 10000);
-}*/
-document.getElementById("audioCapture").addEventListener("click", imageCapture);
+
+
+document.getElementById("audioCapture").addEventListener("click", audioCapture);
 
   //cordova.InAppBrowser.open('http://192.168.1.6:7777', '_self', 'location=yes,hidden=no');
   var conn = new WebSocket('ws://192.168.1.5:7777');
@@ -52,7 +37,7 @@ document.getElementById("audioCapture").addEventListener("click", imageCapture);
     switch(data.type) {
        case "door_bell":
         console.log('someone visit');
-      //  alert('someone visit');
+    alert('someone visit');
         //var data = JSON.parse(msg.data);
 
 //         var media = new Media('https://signellingsvr360owl.herokuapp.com/doorbell.mp3', mediaSuccess, function(e){alert(e);});
@@ -80,21 +65,23 @@ document.getElementById("audioCapture").addEventListener("click", imageCapture);
 
       };
 
-      function imageCapture() {
-         var options = {
-            limit: 1
-         };
-         navigator.device.capture.captureImage(onSuccess, onError, options);
+      function audioCapture() {
+        alert('dsfsdf');
+   var options = {
+      limit: 1,
+      duration: 10
+   };
+   navigator.device.capture.captureAudio(onSuccess, onError, options);
 
-         function onSuccess(mediaFiles) {
-            var i, path, len;
-            for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-               path = mediaFiles[i].fullPath;
-               console.log(mediaFiles);
-            }
-         }
-
-         function onError(error) {
-            navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-         }
+   function onSuccess(mediaFiles) {
+      var i, path, len;
+      for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+         path = mediaFiles[i].fullPath;
+         console.log(mediaFiles);
       }
+   }
+
+   function onError(error) {
+      navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+   }
+}
